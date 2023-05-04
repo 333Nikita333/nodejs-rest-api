c85e674e7d49b9513bc0f27a8e228116200fc638
333Nikita333 <nikitosfox333@gmail.com>
2023-05-05 00:34:56 +0300

    added functions for processing http requests, added routing for working with contacts

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,25 +1,60 @@
-const express = require('express')
+const express = require("express");
+const router = express.Router();
+const contacts = require("../../models/contacts");
+const { HttpError } = require("../../helpers");
 
-const router = express.Router()
+router.get("/", async (req, res, next) => {
+  try {
+    const result = await contacts.listContacts();
+    res.status(200).json(result);
+  } catch (error) {
+    next(error);
+  }
+});
 
-router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.get("/:contactId", async (req, res, next) => {
+  try {
+    const { contactId } = req.params;
+    const result = await contacts.getContactById(contactId);
 
-router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+    if (!result) throw HttpError(404, "Not found");
 
-router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+    res.status(200).json(result);
+  } catch (error) {
+    next(error);
+  }
+});
 
-router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.post("/", async (req, res, next) => {
+  try {
+    const result = await contacts.addContact(req.body);
 
-router.put('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+    if (!result) throw HttpError(400, "missing required name field");
 
-module.exports = router
+    res.status(201).json(result);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.delete("/:contactId", async (req, res, next) => {
+  const { contactId } = req.params;
+  const result = await contacts.removeContact(contactId);
+
+  if (!result) throw HttpError(404, "Not found");
+
+  res.status(200).json({ message: "contact deleted" });
+});
+
+router.put("/:contactId", async (req, res, next) => {
+  const { contactId } = req.params;
+  const body = req.body;
+  if (!body) throw HttpError(400, "missing fields");
+
+  const result = await contacts.updateContact(contactId, body);
+  if (!result) throw HttpError(404, "Not found");
+
+  res.status(200).json(result);
+});
+
+module.exports = router;
